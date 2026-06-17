e6222a9925e283718709b7e20bc8d0238a5b918c
Bennett <calanb90@hotmail.com>
2026-06-16 22:50:37 -0500

    Day 2 additional work

--- Practice/javascript-refresh/day2.js
+++ Practice/javascript-refresh/day2.js
@@ -25,3 +25,61 @@ console.log(`Block Pay: $${blockPay}`);
 console.log(`Miles: ${milesDriven}`);
 console.log(`Fuel Cost: $${fuelCost}`);
 console.log(`Estimated Profit: $${profit}`);
+
+
+const routes = [
+    { pay: 89.50, miles: 65 },
+    { pay: 72.00, miles: 40 },
+    { pay: 110.50, miles: 90 }
+];
+
+
+function totalPay (routes){
+   let totalPay = 0;
+
+    routes.forEach(function (route){
+        totalPay += route.pay;
+    })
+
+    return totalPay;
+}
+
+function averagePay (routes) {
+    return totalPay(routes)/routes.length;
+}
+
+function totalMiles(routes){
+    let totalMiles = 0;
+
+    routes.forEach(function (route){
+        totalMiles += route.miles;
+    })
+
+    return totalMiles;
+}
+
+function averageMiles (routes){
+    return totalMiles(routes)/routes.length;
+}
+
+function highestPayingRoute(routes){
+    let highestPay = 0;
+    let index = null;
+
+    for (let counter = 0; counter < routes.length; counter++){
+        if(routes[counter].pay > highestPay){
+            highestPay = routes[counter].pay;
+            index = counter;
+        }
+    }
+
+    return index;
+}
+
+const mostProfit = routes[highestPayingRoute(routes)];
+
+console.log(`Total Pay:\t$${totalPay(routes)}`);
+console.log(`Average Pay:\t$${averagePay(routes).toFixed(2)}`);
+console.log(`Total Miles:\t${totalMiles(routes)}`);
+console.log(`Average Miles:\t${averageMiles(routes)}`);
+console.log(`The most profitable route was:\t$${mostProfit.pay} with a total of ${mostProfit.miles} miles`);
